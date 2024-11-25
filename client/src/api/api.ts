1105aa8c04a3b55703da13c8f0833c6484b6b182
kryptoish <krish_thakur7@icloud.com>
2024-11-24 22:24:02 -0800

    production server works

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080', // Vite uses `import.meta.env`
+  baseURL: import.meta.env.VITE_API_URL || 'https://ubc-events-finder.onrender.com/', // Vite uses `import.meta.env`
 });
 
 export const getGreeting = async () => {
-  const response = await api.get('/api/greeting');
+  const response = await api.get('/api/main');
   return response.data;
 };
