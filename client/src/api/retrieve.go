3fede56ddbaf36f785b3b6edc5a57e8397c056ef
Quinn <qj.senych@gmail.com>
2024-11-29 16:19:30 -0800

    documentation galore

--- client/src/api/retrieve.go
+++ client/src/api/retrieve.go
@@ -9,76 +9,70 @@ import (
 	"net/http"
 )
 
-/**
-* Takes 2 strings, the token for the account, and the username
-*
-* Currently just prints the retrieved info and doesn't return anything
-*/
-func retrieve_post_data(access_token, userID string, w http.ResponseWriter) MediaResponse {
-	/* get user ID */
-	mediaURL := fmt.Sprintf("https://graph.instagram.com/v21.0/%s/media?fields=id,caption,media_url,permalink&access_token=%s", userID, access_token)
-	mediaResponse, err := http.Get(mediaURL)
+/* From an accessToken, returns the userID and username 
+	Requires a response writer to pass http errors back */
+func retrieveUserId(accessToken string, w http.ResponseWriter) (string, string) {
+	userIdUrl := fmt.Sprintf("https://graph.instagram.com/v21.0/me?fields=id,username&access_token=%s", accessToken)
+	response, err := http.Get(userIdUrl)
 	if err != nil {
-		error_msg := fmt.Sprintf("Error getting media: %v\n", err)
-        http.Error(w, error_msg, http.StatusBadRequest)
-		//log.Fatalf("Error getting media: %v", err)
-	}
-	defer mediaResponse.Body.Close()
-
-	if mediaResponse.StatusCode != http.StatusOK {
-		body, _ := ioutil.ReadAll(mediaResponse.Body)
-		error_msg := fmt.Sprintf("Error response getting media: %s\n", body)
-        http.Error(w, error_msg, http.StatusBadRequest)
-		//log.Fatalf("Error response getting media: %s", body)
-	}
-
-	mediaBody, err := ioutil.ReadAll(mediaResponse.Body)
-	if err != nil {
-		error_msg := fmt.Sprintf("Error reading media response body: %v\n", err)
-        http.Error(w, error_msg, http.StatusBadRequest)
-		//log.Fatalf("Error reading media response body: %v", err)
-	}
-
-	var mediaData MediaResponse
-	if err := json.Unmarshal(mediaBody, &mediaData); err != nil {
-		error_msg := fmt.Sprintf("Error parsing media response: %v\n", err)
-        http.Error(w, error_msg, http.StatusBadRequest)
-		//log.Fatalf("Error parsing media response: %v", err)
-	}
-
-	return mediaData
-}
-
-func retrieve_user_id(access_token string, w http.ResponseWriter) (string, string) {
-	user_id_url := fmt.Sprintf("https://graph.instagram.com/v21.0/me?fields=id,username&access_token=%s", access_token)
-	response, err := http.Get(user_id_url)
-	if err != nil {
-		error_msg := fmt.Sprintf("Error getting user ID: %v\n", err)
-        http.Error(w, error_msg, http.StatusBadRequest)
+		errorMsg := fmt.Sprintf("Error getting user ID: %v\n", err)
+        http.Error(w, errorMsg, http.StatusBadRequest)
 	}
 	defer response.Body.Close()
 
 	if response.StatusCode != http.StatusOK {
 		body, _ := io.ReadAll(response.Body)
-		error_msg := fmt.Sprintf("Error response getting user ID: %s\n", body)
-        http.Error(w, error_msg, http.StatusBadRequest)
-		//log.Fatalf("Error response getting user ID: %s", body)
+		errorMsg := fmt.Sprintf("Error response getting user ID: %s\n", body)
+        http.Error(w, errorMsg, http.StatusBadRequest)
 	}
 
-	var response_decoded map[string]interface{}	
-	if err := json.NewDecoder(response.Body).Decode(&response_decoded); err != nil {
-		error_msg := fmt.Sprintf("Error decoding response: %v\n", err)
-        http.Error(w, error_msg, http.StatusBadRequest)
-		//log.Fatalf("Error decoding response: %v", err)
+	var responseDecoded map[string]interface{}	
+	if err := json.NewDecoder(response.Body).Decode(&responseDecoded); err != nil {
+		errorMsg := fmt.Sprintf("Error decoding response: %v\n", err)
+        http.Error(w, errorMsg, http.StatusBadRequest)
 	}
 
-	user_id, ok := response_decoded["id"].(string)
+	userId, ok := responseDecoded["id"].(string)
 	if !ok {
-		error_msg := fmt.Sprintf("Error parsing user ID response: %v\n", err)
-        http.Error(w, error_msg, http.StatusBadRequest)
-		//log.Fatalf("Error parsing user ID response: %v", err)
+		errorMsg := fmt.Sprintf("Error parsing user ID response: %v\n", err)
+        http.Error(w, errorMsg, http.StatusBadRequest)
 	}	
-	username, _ := response_decoded["username"].(string)
+	username, _ := responseDecoded["username"].(string)
 
-	return user_id, username
+	return userId, username
+}
+
+
+/* From an accessToken and a userID, returns a MediaResponse 
+	containing all the post information from the user.
+	Requires a response writer to pass http errors back */
+func retrievePostData(accessToken, userID string, w http.ResponseWriter) MediaResponse {
+	/* get user ID */
+	mediaURL := fmt.Sprintf("https://graph.instagram.com/v21.0/%s/media?fields=id,caption,media_url,permalink&access_token=%s", userID, accessToken)
+	mediaResponse, err := http.Get(mediaURL)
+	if err != nil {
+		errorMsg := fmt.Sprintf("Error getting media: %v\n", err)
+        http.Error(w, errorMsg, http.StatusBadRequest)
+	}
+	defer mediaResponse.Body.Close()
+
+	if mediaResponse.StatusCode != http.StatusOK {
+		body, _ := ioutil.ReadAll(mediaResponse.Body)
+		errorMsg := fmt.Sprintf("Error response getting media: %s\n", body)
+        http.Error(w, errorMsg, http.StatusBadRequest)
+	}
+
+	mediaBody, err := ioutil.ReadAll(mediaResponse.Body)
+	if err != nil {
+		errorMsg := fmt.Sprintf("Error reading media response body: %v\n", err)
+        http.Error(w, errorMsg, http.StatusBadRequest)
+	}
+
+	var mediaData MediaResponse
+	if err := json.Unmarshal(mediaBody, &mediaData); err != nil {
+		errorMsg := fmt.Sprintf("Error parsing media response: %v\n", err)
+        http.Error(w, errorMsg, http.StatusBadRequest)
+	}
+
+	return mediaData
 }
